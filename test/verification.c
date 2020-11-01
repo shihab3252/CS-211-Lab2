c3c871e41017dc049a5868f884fd721c27fac232
Shihab Rashid <shihab.rashid12@gmail.com>
2020-10-31 23:14:21 -0700

    fsd

--- test/verification.c
+++ test/verification.c
@@ -70,7 +70,7 @@ int test_all_lu_functions()
         t0 = get_sec();    
         block_lu(A3, B3, n,  block_size);
         t1 = get_sec();
-        printf("Elapsed time, block LU: %lf seconds\n", t1 - t0);
+        printf("Elapsed time, block LU: %lf seconds, BlockSize: %d\n", t1 - t0, block_size);
 
         if (verify_matrix(A1, A3, n, n) || verify_matrix(B1, B3, n, 1))
             printf("my block LU is incorrect.\n");
