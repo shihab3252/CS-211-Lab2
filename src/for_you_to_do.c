a28b28bb36013541ac7da882dfaa84a115953fc0
Shihab Rashid <shihab.rashid12@gmail.com>
2020-10-31 20:45:29 -0700

    davyvan

--- src/for_you_to_do.c
+++ src/for_you_to_do.c
@@ -28,56 +28,56 @@ int get_block_size(){
 
 int mydgetrf(double *A, int *ipiv, int n) 
 {
-    /* add your code here */
-    int i, j, k;
-    double* tmpr = (double*)malloc(sizeof(double) * n);
+    int i, maxIndex;
+    double max;
+    double *temprow = (double*) malloc(sizeof(double) * n);
     for (i = 0; i < n; i++)
     {
-        int maxidx = i;
-        double max = fabs(A[i * n + i]);
-        for (j = i + 1; j < n; j++)
+        // pivoting
+        maxIndex = i;
+        max = fabs(A[i*n + i]);
+        
+        int j;
+        for (j = i+1; j < n; j++)
         {
-            double tmp = fabs(A[j * n + i]);
-            if (tmp - max > 1e-6)
+            if (fabs(A[j*n + i]) > max)
             {
-                maxidx = j;
-                max = tmp;
+                maxIndex = j;
+                max = fabs(A[j*n + i]);
+            }
+        }
+        if (max == 0)
+        {
+            printf("LU factorization failed: coefficient matrix is singular.\n");
+            return -1;
+        }
+        else
+        {
+            if (maxIndex != i)
+            {
+                // save pivoting information
+                int temp = ipiv[i];
+                ipiv[i] = ipiv[maxIndex];
+                ipiv[maxIndex] = temp;
+                // swap rows
+                memcpy(temprow, A + i*n, n * sizeof(double));
+                memcpy(A + i*n, A + maxIndex*n, n * sizeof(double));
+                memcpy(A + maxIndex*n, temprow, n * sizeof(double));
             }
         }
 
-        //too small pivot is also unacceptable
-        if (fabs(max - 0.0) < 1e-3) 
-            return -1;
-
-        if (maxidx != i)
+        // factorization
+        for (j = i+1; j < n; j++)
         {
-            //ipiv[maxidx] = ipiv[maxidx] ^ ipiv[i];
-            //ipiv[i] = ipiv[maxidx] ^ ipiv[i];
-            //ipiv[maxidx] = ipiv[maxidx] ^ ipiv[i];
-            int temp = ipiv[i];
-            ipiv[i] = ipiv[maxidx];
-            ipiv[maxidx] = temp;
-            // swap rows
-            memcpy(tmpr, A + i*n, n * sizeof(double));
-            memcpy(A + i*n, A + maxidx*n, n * sizeof(double));
-            memcpy(A + maxidx*n, tmpr, n * sizeof(double));
-
-            //swap(A, tmpr, n, i, maxidx);
-        }
-
-        for (j = i + 1; j < n; j++)
-        {
-            A[j * n + i] = A[j * n + i] / A[i * n + i];
-            double A_j = A[j * n + i];
-            for (k = i + 1; k < n; k++)
+            A[j*n + i] = A[j*n + i] / A[i*n + i];
+            int k;
+            for (k = i+1; k < n; k++)
             {
-                A[j * n + k] -= A_j * A[i * n + k];
+                A[j*n + k] -= A[j*n +i] * A[i*n + k];
             }
         }
     }
-    free(tmpr);
-    return 0;
-
+    free(temprow);
     return 0;
 }
 
@@ -111,7 +111,38 @@ int mydgetrf(double *A, int *ipiv, int n)
 void mydtrsv(char UPLO, double *A, double *B, int n, int *ipiv)
 {
     /* add your code here */
-    return;
+    double *y = (double*) malloc(n * sizeof(double));
+    int i, j;
+    double sum;
+    if (UPLO == 'L')
+    {
+        y[0] = B[ipiv[0]];
+        for (i = 1; i < n; i++)
+        {
+            sum = 0.0;
+            for (j = 0; j < i; j++)
+            {
+                sum += y[j] * A[i*n + j];
+            }
+            y[i] = B[ipiv[i]] - sum;
+        }
+    }
+    else if (UPLO == 'U')
+    {
+        y[n - 1] = B[n - 1] / A[(n-1)*n + n-1];
+        for (i = n-2; i >= 0; i--)
+        {
+            sum = 0;
+            for (j = i+1; j < n; j++)
+            {
+                sum += y[j] * A[i*n + j];
+            }
+            y[i] = (B[i] - sum) / A[i*n + i];
+        }
+    }
+
+    memcpy(B, y, sizeof(double) * n);
+    free(y);
 }
 
 /**
@@ -123,7 +154,66 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
 {
     /* add your code here */
     /* please just copy from your lab1 function optimal( ... ) */
-    return;
+    int i1 = i, j1 = j, k1 = k;
+    int ni = i + b > n ? n : i + b;
+    int nj = j + b > n ? n : j + b;
+    int nk = k + b > n ? n : k + b;
+
+    for (i1 = i; i1 < ni; i1 += 3)
+    {
+        for (j1 = j; j1 < nj; j1 += 3)
+        {
+            int t = i1 * n + j1;
+            int tt = t + n;
+            int ttt = tt + n;
+            register double c00 = C[t];
+            register double c01 = C[t + 1];
+            register double c02 = C[t + 2];
+            register double c10 = C[tt];
+            register double c11 = C[tt + 1];
+            register double c12 = C[tt + 2];
+            register double c20 = C[ttt];
+            register double c21 = C[ttt + 1];
+            register double c22 = C[ttt + 2];
+
+            for (k1 = k; k1 < nk; k1 += 3)
+            {
+		int l;
+                for (l = 0; l < 3; l++)
+                {
+                    int ta = i1 * n + k1 + l;
+                    int tta = ta + n;
+                    int ttta = tta + n;
+                    int tb = k1 * n + j1 + l * n;
+                    register double a0 = A[ta];
+                    register double a1 = A[tta];
+                    register double a2 = A[ttta];
+                    register double b0 = B[tb];
+                    register double b1 = B[tb + 1];
+                    register double b2 = B[tb + 2];
+
+                    c00 -= a0 * b0;
+                    c01 -= a0 * b1;
+                    c02 -= a0 * b2;
+                    c10 -= a1 * b0;
+                    c11 -= a1 * b1;
+                    c12 -= a1 * b2;
+                    c20 -= a2 * b0;
+                    c21 -= a2 * b1;
+                    c22 -= a2 * b2;
+                }
+            }
+            C[t] = c00;
+            C[t + 1] = c01;
+            C[t + 2] = c02;
+            C[tt] = c10;
+            C[tt + 1] = c11;
+            C[tt + 2] = c12;
+            C[ttt] = c20;
+            C[ttt + 1] = c21;
+            C[ttt + 2] = c22;
+        }
+    }
 }
 
 /**
@@ -156,6 +246,82 @@ void mydgemm(double *A, double *B, double *C, int n, int i, int j, int k, int b)
  **/
 int mydgetrf_block(double *A, int *ipiv, int n, int b) 
 {
+    int ib, i, j, k, maxIndex;
+    double max, sum;
+    double *temprow = (double*) malloc(sizeof(double) * n);
+
+    for (ib = 0; ib < n; ib += b)
+    {
+        for (i = ib; i < ib+b && i < n; i++)
+        {
+            // pivoting
+            maxIndex = i;
+            max = fabs(A[i*n + i]);
+            
+            int j;
+            for (j = i+1; j < n; j++)
+            {
+                if (fabs(A[j*n + i]) > max)
+                {
+                    maxIndex = j;
+                    max = fabs(A[j*n + i]);
+                }
+            }
+            if (max == 0)
+            {
+                printf("LU factorization failed: coefficient matrix is singular.\n");
+                return -1;
+            }
+            else
+            {
+                if (maxIndex != i)
+                {
+                    // save pivoting information
+                    int temp = ipiv[i];
+                    ipiv[i] = ipiv[maxIndex];
+                    ipiv[maxIndex] = temp;
+                    // swap rows
+                    memcpy(temprow, A + i*n, n * sizeof(double));
+                    memcpy(A + i*n, A + maxIndex*n, n * sizeof(double));
+                    memcpy(A + maxIndex*n, temprow, n * sizeof(double));
+                }
+            }
+
+            // factorization
+            for (j = i+1; j < n; j++)
+            {
+                A[j*n + i] = A[j*n + i] / A[i*n + i];
+                int k;
+                for (k = i+1; k < ib+b && k < n; k++)
+                {
+                    A[j*n + k] -= A[j*n +i] * A[i*n + k];
+                }
+            }
+        }
+
+        // update A(ib:end, end+1:n)
+        for (i = ib; i < ib+b && i < n; i++)
+        {
+            for (j = ib+b; j < n; j++)
+            {
+                sum = 0;
+                for (k = ib; k < i; k++)
+                {
+                    sum += A[i*n + k] * A[k*n + j];
+                }
+                A[i*n + j] -= sum;
+            }
+        }
+
+        // update A(end+1:n, end+1:n)
+        for (i = ib+b; i < n; i += b)
+        {
+            for (j = ib+b; j < n; j += b)
+            {
+                mydgemm(A, A, A, n, i, j, ib, b);
+            }
+        }
+    }
     return 0;
 }
 
